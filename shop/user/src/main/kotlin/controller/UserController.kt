ea567abfdbbc8a9fc11bbfa25dd3f14561c7790b
정주헌 <cherish8513@naver.com>
2022-04-23 21:40:42 +0900

    feat: User Service 간단한 Get Mapping 추가

--- shop/user/src/main/kotlin/controller/UserController.kt
+++ shop/user/src/main/kotlin/controller/UserController.kt
@@ -1,13 +1,18 @@
 package shop.user.main.controller
 
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import shop.user.main.vo.Greeting
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/")
-class UserController {
+class UserController(val greeting: Greeting) {
 
-    @PostMapping("/health-check")
-    fun status() : String = "It's Working in User Service"
+    @GetMapping("/health-check")
+    fun status(servletRequest: HttpServletRequest) : String = "It's Working in User Service " + servletRequest.serverPort
+
+    @GetMapping("welcome")
+    fun welcome() : String = greeting.message
 }
---
+++ shop/user/src/main/kotlin/vo/Greeting.kt
@@ -0,0 +1,10 @@
+package shop.user.main.vo
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class Greeting {
+    @Value("\${greeting.message}")
+    lateinit var message: String
+}
