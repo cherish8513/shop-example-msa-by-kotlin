f9938adb7250a12e582abba186a15f9f1025136e
정주헌 <cherish8513@naver.com>
2022-05-03 12:22:20 +0900

    feat: actuator 설정 yml 기능 추가

--- shop/eureka-server/build.gradle.kts
+++ shop/eureka-server/build.gradle.kts
@@ -2,8 +2,8 @@ extra["springCloudVersion"] = "2021.0.1"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
-
 dependencyManagement {
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
