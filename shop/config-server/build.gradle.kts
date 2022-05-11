fa0030939e1ac63f8a7ede0e964bded1b9e25e32
정주헌 <cherish8513@naver.com>
2022-05-11 12:56:52 +0900

    feat: message bus 디펜던시 추가

--- shop/config-server/build.gradle.kts
+++ shop/config-server/build.gradle.kts
@@ -2,6 +2,8 @@ extra["springCloudVersion"] = "2021.0.1"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 dependencyManagement {
--- shop/config-server/src/main/kotlin/ConfigServerApplication.kt
+++ shop/config-server/src/main/kotlin/ConfigServerApplication.kt
@@ -1,10 +1,8 @@
 package shop.config.main
 
-import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
-import org.springframework.context.annotation.Bean
 
 
 @SpringBootApplication
